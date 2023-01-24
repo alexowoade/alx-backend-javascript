64a5990a783b01af4eb58f7e0d155606820712ab
Obelem <ibbraidejesse@gmail.com>
2023-01-24 13:20:52 +0100

    refactored code with destructuring

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,11 +3,5 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const res = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  const array = [];
-  res.forEach((response) => {
-    if (response.status === 'fulfilled') {
-      array.push({ status: response.status, value: response.value });
-    } else { array.push({ status: response.status, value: response.reason }); }
-  });
-  return array;
+  return res.map(({ status, value, reason }) => ({ status, value: status === 'fulfilled' ? value : reason }));
 }
