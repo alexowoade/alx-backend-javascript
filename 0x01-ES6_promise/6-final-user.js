111e218895eb4916ceda3acb5b7dbf1a6dd42fc9
Obelem <ibbraidejesse@gmail.com>
2023-01-24 19:10:42 +0100

    refactored code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((responses) => {
-      const array = [];
-      responses.forEach((response) => {
-        if (response.status === 'fulfilled') {
-          array.push({ status: response.status, value: response.value });
-        } else array.push({ status: response.status, value: response.reason });
-      });
-      return array;
-    });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const signUp = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
+
+  try {
+    const resps = await Promise.all([signUp, upload]);
+    return resps;
+  } catch (error) {
+    return error;
+  }
 }
