845cf5a59692f4934285c64449fc8db15d742488
Obelem <ibbraidejesse@gmail.com>
2023-01-24 19:27:28 +0100

    added try catch block

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,33 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const signupPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = {
+    status: 'pending ',
+  };
+  const promise2 = {
+    status: 'pending ',
+  };
 
-  const resolvedPromises = await Promise.all([signupPromise, uploadPromise]);
+  try {
+    const signup = await signUpUser(firstName, lastName);
+    promise1.status = 'fulfilled';
+    promise1.value = signup;
+  } catch (err) {
+    promise1.status = 'rejected';
+    promise1.value = err.toString();
+  }
 
-  const [signup, upload] = resolvedPromises;
-  return [{ status: 'fulfilled', value: signup }, { status: 'fulfilled', value: upload }];
+  try {
+    const upload = await uploadPhoto(fileName);
+    promise2.status = 'fulfilled';
+    promise2.value = upload;
+  } catch (err) {
+    promise2.status = 'rejected';
+    promise2.value = err.toString();
+  }
+
+  return [promise1, promise2];
 }
+
+export default handleProfileSignup;
