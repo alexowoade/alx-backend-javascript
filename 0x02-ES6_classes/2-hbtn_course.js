fa249913f33d2a47905bbd0572be246399f1eddf
Obelem <ibbraidejesse@gmail.com>
2023-01-29 22:49:45 +0100

    debugging

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -21,7 +21,7 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if (typeof length !== 'number') throw new TypeError('Length must be a string');
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
     this._length = length;
   }
 
@@ -31,13 +31,9 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (
-      Array.isArray(students)
-      && students.every((s) => typeof s === 'string')
-    ) {
-      this._students = students;
-    } else {
+    if (!Array.isArray(students) || students.every((student) => typeof student !== 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
+    this._students = students;
   }
 }
