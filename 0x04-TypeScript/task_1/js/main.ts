0311f09352ca0ea9ec1f5ad5ab74efde07873ffb
Obelem <ibbraidejesse@gmail.com>
2023-02-04 23:32:17 +0100

    basic oop with typescript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,6 +6,7 @@ interface Teacher {
   [key: string]: any;
 }
 
+// task 1 - Let's build a Teacher interface
 const teacher3: Teacher = {
   firstName: "John",
   fullTimeEmployee: false,
@@ -13,3 +14,35 @@ const teacher3: Teacher = {
   location: "London",
   contract: false,
 };
+
+// task 2 - Extending the Teacher class
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// task 3 - Printing teachers
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = function (firstName, lastName) {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+// task 4 - Writing a class
+interface Student {
+  firstName: string;
+  lastName: string;
+}
+
+class StudentClass implements Student {
+  constructor(public firstName: string, public lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
