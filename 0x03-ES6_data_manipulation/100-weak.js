c78455cf877e1d5e680fbda4de105c09313e55c4
alexowoade <alexowoade@gmail.com>
2023-04-26 00:30:55 +0000

    alex

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,21 +1,10 @@
-// Create a WeakMap instance to store endpoint query counts
 const weakMap = new WeakMap();
 
-// Define a function to track API queries for each endpoint
-function queryAPI(endpoint) {
-  // Retrieve the current query count for the endpoint, or set it to 0 if not yet tracked
-  let count = weakMap.get(endpoint) || 0;
-  
-  // Throw an error if the query count is greater than or equal to 5
-  if (count >= 5) {
+export function queryAPI(endpoint) {
+  const queryCount = weakMap.get(endpoint) || 0;
+  if (queryCount >= 5) {
     throw new Error('Endpoint load is high');
   }
-  
-  // Increment the query count and set it back in the WeakMap
-  weakMap.set(endpoint, ++count);
+  weakMap.set(endpoint, queryCount + 1);
 }
-
-// Export the weakMap and queryAPI function for use in other modules
-export { weakMap, queryAPI };
-
 
